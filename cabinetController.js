6e104ae451b5b3a9e55e143f18ee0ec4debbbbd0
Cristian Avina Lopez <cavinalopez@cpp.edu>
2016-06-25 23:18:03 -0700

    transitioned from list to regular column for binding

--- cabinetController.js
+++ cabinetController.js
@@ -19,6 +19,26 @@ $scope.list= [
 		color:'Chestnut',
 		snippet:'Assembled Dark Chestnut 10x10 Kitchen',
 		age: '3'
+	}, {
+       	image: 'http://buycabinetdirect.com/media/wysiwyg/CHB-LEVEL3.jpg',
+    	type: 'Assembled',
+		color:'Chocolate' ,
+		snippet:'Assembled Chocolate Glaze 10x10 Kitchen',
+		age: '4'
+	}, {
+		image: 'http://buycabinetdirect.com/media/wysiwyg/CA-LEVEL2.jpg',
+    	type: 'Assembled',
+		color:'Cherry' ,
+		snippet:'Assembled Cherry Antique 10x10 Kitchen',
+		age: '5'
+	}, {
+		image: 'http://buycabinetdirect.com/media/wysiwyg/CNS-LEVEL1.jpg',
+    	type: 'Assembled',
+		color:'Cinnamon' ,
+		snippet:'Assembled Cinnamon Shaker 10x10 Kitchen',
+		age: '6'
 	}] 
 });
+
+
 
